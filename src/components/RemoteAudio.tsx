a0815c514ecda713778b2d1a3353f6263c899746
sako0 <y.m1911a1.0921@gmail.com>
2023-04-21 18:39:12 +0900

    audio-levelの修正

--- src/components/RemoteAudio.tsx
+++ src/components/RemoteAudio.tsx
@@ -1,4 +1,7 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+
+// クライアントサイドでのみ AudioContext を作成
+const audioContext = typeof window !== "undefined" ? new AudioContext() : null;
 
 interface RemoteAudioProps {
   userId: string;
@@ -14,12 +17,56 @@ const RemoteAudio: React.FC<RemoteAudioProps> = ({
   onVolumeChange,
 }) => {
   const audioRef = useRef<HTMLAudioElement>(null);
-  // const audioLevel = useAudioLevelMonitor({ stream });
-  // console.log(audioLevel);
+  const [talkingLevel, setTalkingLevel] = useState(0);
+
+  useEffect(() => {
+    if (!stream) return;
+    if (!audioRef.current) return;
+    if (!audioContext) return;
+
+    const analyser = audioContext.createAnalyser();
+    const audioSource = audioContext.createMediaStreamSource(stream);
+    const destination = audioContext.createMediaStreamDestination();
+
+    audioSource.connect(analyser);
+    analyser.connect(destination);
+    audioRef.current.srcObject = destination.stream;
+
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+    analyser.smoothingTimeConstant = 0.3;
+    analyser.fftSize = 2048;
+
+    const updateAudioLevel = () => {
+      analyser.getByteTimeDomainData(dataArray);
+
+      let sum = 0;
+      for (let i = 0; i < bufferLength; i++) {
+        const difference = dataArray[i] - 128;
+        sum += difference * difference;
+      }
+      const rms = Math.sqrt(sum / bufferLength);
+      const thresholds = [1, 2, 4, 5]; // 5段階の音量しきい値
+      const level = thresholds.findIndex((threshold) => rms < threshold);
+      setTalkingLevel(level);
+      requestAnimationFrame(updateAudioLevel);
+    };
+
+    updateAudioLevel();
+
+    return () => {
+      if (audioSource) audioSource.disconnect(analyser);
+      if (analyser) analyser.disconnect(destination);
+      if (destination) destination.disconnect();
+    };
+  }, [stream]);
 
   useEffect(() => {
     if (audioRef.current) {
       audioRef.current.srcObject = stream;
+      audioRef.current.play().catch((error) => {
+        console.error("Error playing audio:", error);
+      });
     }
   }, [stream]);
 
@@ -32,11 +79,15 @@ const RemoteAudio: React.FC<RemoteAudioProps> = ({
   const handleVolumeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     onVolumeChange(userId, parseFloat(event.target.value));
   };
+  const greenIntensity = Math.floor((255 / 4) * talkingLevel);
+  const textColor = `rgb(0, ${greenIntensity}, 0)`;
 
   return (
     <div>
-      <audio ref={audioRef} autoPlay />
-      <label htmlFor={`${userId}-volume`}>Volume:</label>
+      <audio ref={audioRef} />
+      <p className={"text-sm "} style={{ color: textColor }}>
+        {userId}
+      </p>
       <input
         id={`${userId}-volume`}
         type="range"
