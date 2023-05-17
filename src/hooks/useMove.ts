135d325a33715dc4c584863cd1b81c8aafa7e961
sako <57947664+sako0@users.noreply.github.com>
2023-05-17 09:04:06 +0900

    エリアとmoveのwebsocket構築 (#21)
    * エリアとmoveのwebsocket構築
    
    * 適切にmoveが行えるように修正

--- src/hooks/useMove.ts
+++ src/hooks/useMove.ts
@@ -1,54 +1,29 @@
-import { useState, useEffect, MutableRefObject, useCallback } from "react";
+import { MutableRefObject, useCallback } from "react";
 
 type UseMoveProps = {
-  areaID: number;
-  fromUserID: number;
   socket: MutableRefObject<WebSocket | null>;
-  connectWebSocket: () => void;
+  areaID: number;
+  currentUserID: number;
 };
-
-type Message = {
-  type: string;
-  fromUserID: number;
-  toUserID: number;
-  roomID: number;
-  connectedUserIds: number[];
-  xAxis: number;
-  yAxis: number;
-};
-
 export const useMove = (props: UseMoveProps) => {
-  const { areaID, fromUserID, socket, connectWebSocket } = props;
-  const [xAxis, setXAxis] = useState(0);
-  const [yAxis, setYAxis] = useState(0);
+  const { socket, areaID, currentUserID } = props;
 
-  const move = useCallback(() => {
-    connectWebSocket();
-    if (socket.current) {
-      socket.current.onopen = () => {
-        if (!socket.current) return;
+  const move = useCallback(
+    (yAxis: number, xAxis: number) => {
+      if (socket.current) {
         socket.current.send(
           JSON.stringify({
             type: "move",
             areaID: areaID,
-            fromUserID: fromUserID,
+            fromUserID: currentUserID,
             xAxis: xAxis,
             yAxis: yAxis,
           })
         );
-      };
-      socket.current.onmessage = (event) => {
-        const data: Message = JSON.parse(event.data);
-        const { type, fromUserID, toUserID, xAxis, yAxis } = data;
-        if (type === "move") {
-          console.log("move", data);
-        }
-      };
-      socket.current.onerror = (error) => {
-        console.error("WebSocket error:", error);
-      };
-    }
-  }, [connectWebSocket, socket, areaID, fromUserID, xAxis, yAxis]);
+      }
+    },
+    [areaID, currentUserID, socket]
+  );
 
-  return { move, xAxis, yAxis, setXAxis, setYAxis };
+  return { move };
 };
